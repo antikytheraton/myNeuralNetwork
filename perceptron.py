7fc07d24b46d5f86c10436cdb2edaf2616c7697c
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2017-06-20 10:39:12 -0500

    jupyter notebook

--- perceptron.py
+++ perceptron.py
@@ -1,19 +1,18 @@
 from random import choice
 from numpy import array, dot, random
 
-unit_step = lambda x: 0 if x<0 else 1
+unit_step = lambda x: 0 if x < 0 else 1
 
 training_data = [
 	(array([0,0,1]), 0),
 	(array([0,1,1]), 1),
 	(array([1,0,1]), 1),
-	(array([1,1,1]), 1),
-]
+	(array([1,1,1]), 1), ]
 
 w = random.rand(3)
 
 errors = []
-eta = 0.2
+eta = 0.2 
 n = 100
 
 for i in range(n):
@@ -26,7 +25,3 @@ for i in range(n):
 for x, _ in training_data:
 	result = dot(x, w)
 	print("{}: {} -> {}".format(x[:2], result, unit_step(result)))
-
-from pylab import plot, ylim
-ylim([-1,1])
-plot(errors)
